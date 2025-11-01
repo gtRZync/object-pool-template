8a9bcb21d835702466c0b71de0e09beae4d37544
Myson Dio <sendorfbg@gmail.com>
2025-11-01 01:42:07 -0300

    feat: removing contructor + implementing methods for demo with or without arguments

--- demo/Bullet.cpp
+++ demo/Bullet.cpp
@@ -1,30 +1,51 @@
 #include "Bullet.hpp"
 
-Bullet::Bullet(float x, float y) {
-    bullet.setPosition(x, y);
+Bullet::Bullet() : useSprite(false) {
     bullet.setFillColor(sf::Color(255, 215, 0));
     bullet.setRadius(5.f);
+}
+
+Bullet::Bullet(const sf::Texture& texture, double scale) : useSprite(true) {
+    sprite.setTexture(texture);
+    if(scale > 1.0) {
+        sprite.setScale(scale, scale);
+    }
 }
 
 Bullet::~Bullet() {};
 
 void Bullet::update(float dt) {
-    if(active) {
+    constexpr int THRESHOLD = -3;
+    
+    if(useSprite) {
+        sprite.move(0.f, -(speed * dt));
+        if(sprite.getPosition().y < THRESHOLD) {
+            active = false;
+        }
+    } else {
         bullet.move(0.f, -(speed * dt));
+        if(bullet.getPosition().y < THRESHOLD) {
+            active = false;
+        }
     }
-    if(bullet.getPosition().y < -3) {
-        active = false;
+}
+
+void Bullet::setPosition(int x, int y) {
+    if(useSprite) {
+        sprite.setPosition(x, y);
+    } else {
+        bullet.setPosition(x, y);
     }
 }
 
 void Bullet::reset() {
-    bullet.setPosition(0.f, 0.f);
+    this->setPosition(0.f, 0.f);
     active = false;
 }
 
 void Bullet::fire(int x, int y) {
-    bullet.setPosition(x, y);
+    this->setPosition(x, y);
     active = true;
 }
 
-bool Bullet::isActive() const { return active; }
+bool Bullet::isActive() const { return this->active; }
--- demo/Bullet.hpp
+++ demo/Bullet.hpp
@@ -7,24 +7,28 @@ class Bullet : public sf::Drawable {
         sf::Sprite      sprite;
         float           speed       = 1300.f;
         bool            active;
-        bool            useSprite   = false;
+        bool            useSprite;
     public:
-        explicit Bullet(float x, float y);
+        explicit Bullet(const sf::Texture& texture, double scale = 1.0);
 
-        explicit Bullet(const sf::Texture& texture);
-
-        Bullet() : Bullet(0.f, 0.f) {}
+        Bullet();
         
         ~Bullet();
     public:
         virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
-            target.draw(bullet, states);
+            if(useSprite) {
+                target.draw(sprite, states);
+            } else {
+                target.draw(bullet, states);
+            }
         }
         void update(float dt);
 
         void reset();
 
         void fire(int x, int y);
+
+        void setPosition(int x, int y);
 
         bool isActive() const;
 };
